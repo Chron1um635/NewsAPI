fee42c520840c6247322e48ad04547da7af0a1af
Chron1um635 <chron1um522@gmail.com>
2024-09-22 14:32:12 +0300

    create model

--- NewsAPI/Models/News.swift
+++ NewsAPI/Models/News.swift
@@ -5,4 +5,23 @@
 //  Created by Максим Назаров on 22.09.2024.
 //
 
-import Foundation
+
+struct News: Decodable {
+    let articles: [Article]
+}
+
+struct Article: Decodable {
+    let source: Source
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+
+struct Source: Decodable {
+    let name: String
+}
+

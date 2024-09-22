68b750bd83efec3ab5f0718f42008ecbdbd433ed
Chron1um635 <chron1um522@gmail.com>
2024-09-22 13:19:03 +0300

    Initial Commit

---
+++ NewsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAPI
+//
+//  Created by Максим Назаров on 22.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

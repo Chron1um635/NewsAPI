39af00abe23af1dc0ffca5213e9355d5ce11802a
Chron1um635 <chron1um522@gmail.com>
2024-09-22 14:33:59 +0300

    add network

--- NewsAPI/ViewControllers/ViewController.swift
+++ NewsAPI/ViewControllers/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchNews()
     }
 
 
+    
 }
 
+// MARK: - Network
+private extension ViewController {
+    func fetchNews() {
+        guard let url = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2024-08-22&sortBy=publishedAt&apiKey=dfe5b2f4813e4781a9f27647aa6fd1a7") else { return }
+        URLSession.shared.dataTask(with: url) { data ,_, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            do {
+                let news = try JSONDecoder().decode(News.self, from: data)
+                print(news)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
+}

250d50d46fdfd82e6f860a437c2c44138002ebc5
Chron1um635 <chron1um522@gmail.com>
2024-09-24 22:25:16 +0300

    setup network manager

--- NewsAPI/Services/NetworkManager.swift
+++ NewsAPI/Services/NetworkManager.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+enum NetworkError: String, Error {
+    case noData
+    case decodingError
+}
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchNews(
+        from url: URL,
+        completion: @escaping(
+            Result<
+            News,
+            NetworkError
+            >
+        ) -> Void
+    ) {
+        URLSession.shared.dataTask(with: url) { data ,_, error in
+            guard let data else {
+                print(error ?? "No error description")
+                return
+            }
+            do {
+                let news = try JSONDecoder().decode(News.self, from: data)
+                completion(.success(news))
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+    
+    func fetchImage(
+        from url: URL,
+        completion: @escaping(
+            Result<Data, NetworkError>
+        ) -> Void
+    ) {
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                completion(.failure(.noData))
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(imageData))
+            }
+        }
+    }
+}

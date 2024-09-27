e5800239463cb4a37fd3009727ac35409fc118ea
Chron1um635 <chron1um522@gmail.com>
2024-09-27 20:13:21 +0300

    update model

--- NewsAPI/Models/News.swift
+++ NewsAPI/Models/News.swift
@@ -6,11 +6,27 @@
 //
 
 
-struct News: Decodable {
-    let articles: [Article]
+struct News {
+    var articles: [Article]
+    
+    init(articles: [Article]) {
+        self.articles = articles
+    }
+    
+    init(news: [String: Any]?) {
+        self.articles = []
+        let articles = news?["articles"] as? [[String: Any]]
+        var someArticles: [Article] = []
+        articles?.forEach { article in
+            let art = Article(news: article)
+            someArticles.append(art)
+        }
+        self.articles.append(contentsOf: someArticles)
+    }
+    
 }
 
-struct Article: Decodable {
+struct Article {
     let source: Source
     let author: String?
     let title: String
@@ -18,10 +34,21 @@ struct Article: Decodable {
     let url: String
     let urlToImage: String?
     let publishedAt: String
+    
+    init(news: [String: Any]) {
+        source = news["source"] as? Source ?? Source(name: "")
+        author = news["author"] as? String ?? ""
+        title = news["title"] as? String ?? ""
+        description = news["description"] as? String ?? ""
+        url = news["url"] as? String ?? ""
+        urlToImage = news["urlToImage"] as? String ?? ""
+        publishedAt = news["publishedAt"] as? String ?? ""
+    }
 }
 
 
-struct Source: Decodable {
+struct Source {
     let name: String
+    
 }
 
